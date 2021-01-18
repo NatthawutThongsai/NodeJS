21ff36fed5f4ea6b10bba6d2803b146e755413e3
NatthawutThongsai <natthawut483@gmail.com>
2021-01-18 22:38:43 +0700

    0.3

--- File.js
+++ File.js
@@ -1,3 +1,7 @@
 var fs=require('fs');
 var readFile=fs.readFileSync('code.txt','utf-8')
 console.log(readFile)
+fs.writeFileSync('HelloNodeJS.txt',readFile)
+fs.mkdir('TestWriteFile',function(){
+    fs.writeFileSync('./TestWriteFile/Readme.txt',readFile)
+});
