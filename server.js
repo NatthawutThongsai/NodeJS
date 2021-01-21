66e489f32f86a71e7bdfcf154a3f5cf4c90d5af2
NatthawutThongsai <natthawut483@gmail.com>
2021-01-21 12:15:26 +0700

    3.2

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http=require('http');
+var fs=require('fs');
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("Helloworld");
+    res.writeHead(200,{'Content-Type':'text/html'});
+    //res.end("Helloworld");
+    var myStream=fs.createReadStream(__dirname+"/"+'index.html','utf-8');
+    myStream.pipe(res);
 }).listen(8081,'127.0.0.1');
